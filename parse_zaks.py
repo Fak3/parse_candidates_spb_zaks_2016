771b5a13f6264cfcbf8b9f6f3632759cae5fc932
Evstifeev Roman <someuniquename@gmail.com>
2016-09-12 18:38:25 +0300

    update

--- parse_zaks.py
+++ parse_zaks.py
@@ -16,7 +16,7 @@ def get_html(url):
         print('try %d get %s' % (retry, url))
         response = get(url)
         if response.status_code == 200 and response.text:
-            soup = BeautifulSoup(response.text)
+            soup = BeautifulSoup(response.text, 'html.parser')
             if soup.find('tbody', id='test'):
                 return soup
     print('Max reties exceeded')
